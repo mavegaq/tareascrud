3b6985742f9ae1e3941b9eb8b7e44012afa74418
mavegaq <manuel.93.vega@gmail.com>
2023-05-21 20:22:44 -0600

    Fourth commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors'); /
+const cors = require('cors'); 
 const app = express();
 const todosRouter = require('./routes/todos');
 require('dotenv').config();
